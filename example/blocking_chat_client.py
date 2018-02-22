1128030bd44514609f9790f36c126641312dfb60
land-pack <landpack@sina.com>
2018-02-22 15:35:38 +0800

    add blocking chat client & dispatch server

---
+++ example/blocking_chat_client.py
@@ -0,0 +1,50 @@
+#coding: utf-8
+
+import ujson
+from subprocess import call
+from websocket import create_connection
+
+name = raw_input("Please input your name: ")
+
+host_address = "ws://localhost:9001/ws?name={}".format(name)
+ws = create_connection(host_address)
+
+
+receiver_flag = []
+
+while True:
+   
+    print("try again ...")
+    if not receiver_flag:
+        print("Who you like to chat? ")
+        receivers_at = raw_input("")
+        print("==>>>{}".format(receivers_at))
+        receivers_at = receivers_at.partition(' ')[0]
+        print(receivers_at)
+        receivers_at = receivers_at.split("@")[1]
+        receivers = [ receivers_at,]
+        #receivers = [i.split("@")[1] for i in receivers_at if "@" in receivers_at]
+        print("receivers >>{}".format(receivers))
+        receiver_flag.append(receivers)
+        continue
+    else:
+        input_content = raw_input(">>> \n")
+        
+    
+
+    print("sending ... {}".format(input_content))
+    data = {
+        "receivers":receivers,
+        "msg": input_content
+    }
+
+    data_str = ujson.dumps(data)
+    if input_content:
+        ws.send(data_str)
+
+    recv_content = ws.recv()
+    print("Recv << {}".format(recv_content))
+    call(["say", recv_content])
+    if recv_content == 'bye':
+        ws.close()
+        break
---
+++ example/dispatch_server.py
@@ -0,0 +1,105 @@
+import ujson
+import tornado.websocket
+import tornado.httpserver
+import tornado.ioloop
+from collections import defaultdict
+
+name_users = dict()
+
+class PlayManager(object):
+    name2connection =  dict()
+
+    @classmethod
+    def join(cls, name, conn):
+        cls.name2connection[name] = conn
+
+    @classmethod
+    def leave(cls, name):
+        del cls.name2connection[name]
+
+    @classmethod
+    def pub(cls, msg, white_list=[], black_list=[]):
+
+        mix_list = set(cls.name2connection.keys()) - set(black_list)
+        player_list = white_list if white_list else list(mix_list)
+
+        for name in player_list:
+            try:
+                conn = cls.name2connection.get(name)
+                conn.write_message(msg)
+            except:
+                print("Can not found any play name as {} and connect db current {}".format(name, conn))
+    
+
+    @classmethod
+    def dispatch(cls, data):
+        receivers = data.get("receivers")
+        msg = data.get("msg")
+        cls.pub(msg, white_list=receivers)
+        
+
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    
+    def prepare(self):
+        d = {k:v[0] for k, v in self.request.arguments.iteritems()}
+        self.arg = d
+        print('websocket arguments:%s', d)
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        name = self.arg.get("name")
+        print("Open Connection by  {}".format(name))
+        #name_users[name]= self
+
+        # New player join, publish a join message
+        data = {
+            "tid": "join",
+            "name": name
+        }
+        data_str = ujson.dumps(data)
+        PlayManager.pub(data_str)
+        PlayManager.join(name, self)
+
+
+
+    def on_message(self, message):
+        # self.write_message("Your message was: " + message)
+        # who you want to talk
+        try:
+            data = ujson.loads(message)
+        except:
+            self.write_message("Unable to parser your data structure")
+        else:
+            PlayManager.dispatch(data)
+
+
+
+    def on_close(self):
+        #del name_users[self.arg.name]
+        print("Close Connection")
+
+        # user leave
+        PlayManager.leave(self.arg.get("name"))
+        data = {
+            "tid": "leave",
+            "name": self.arg.get("name")
+        }
+        data_str = ujson.dumps(data)
+        PlayManager.pub(data_str)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/ws', WebSocketHandler),
+        ]
+        tornado.web.Application.__init__(self, handlers)
+
+
+if __name__ == '__main__':
+    ws_app = Application()
+    server = tornado.httpserver.HTTPServer(ws_app)
+    server.listen(9001)
+    tornado.ioloop.IOLoop.instance().start()
